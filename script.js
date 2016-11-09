3ab3dc2d3296f56ab0762a9ac7a8b257491668af
Sara Ahlers <sjahlers@gmail.com>
2016-11-08 20:23:48 -0500

    Updated styling, javascript to produce error alerts

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 var shoppingList = [];
-
+addbutton.onclick = addToList;
 
 //Function to add items to array
 
 function addToList() {
 		var newItem = document.getElementById('item').value;
 		var newPrice = parseFloat(document.getElementById('price').value);
+			
+		
+		if (newItem === "") {
+			alert("Please enter an item");
+		}
+		
+		else if (isNaN(newPrice)) {
+			alert("The price must be a number");
+		}
+		
+		else {
 			var obj = {
 				item: newItem,
 				price: newPrice
 			};
-		shoppingList.push(obj);
-		var printList = document.createElement('li');
-		printList.innerHTML = newItem + ":     $" + newPrice.toFixed(2);
-		document.getElementById('list').appendChild(printList);
+			shoppingList.push(obj);
+			var printList = document.createElement('p');
+			printList.innerHTML = newItem + ":     $" + newPrice.toFixed(2);
+			document.getElementById('list').appendChild(printList);
+		}
 
 		//Get total cost
 		var totalCost = 0;
